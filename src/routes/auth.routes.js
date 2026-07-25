69bb9ec3b83335484e0b9e3fc17709d68e2d5583
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-07-25 17:10:18 +0530

    new

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,9 @@
 const { Router } = require("express");
 const authController = require("../controllers/auth.controller");
+const authMiddleware = require("../middlewares/auth.middleware");
 
 const authRouter = Router();
+
 /**
  * @route POST /api/auth/register
  * @description Register a new user
@@ -22,5 +24,16 @@ authRouter.post("/login", authController.loginUserController);
  * @access public
  */
 authRouter.get("/logout", authController.logoutUserController);
+
+/**
+ * @route GET /api/auth/get-me
+ * @description get the current logged in user details
+ * @access private
+ */
+authRouter.get(
+  "/get-me",
+  authMiddleware.authUser,
+  authController.getMeController,
+);
 
 module.exports = authRouter;
