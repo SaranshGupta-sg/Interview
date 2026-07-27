e414baff0d02b3f87e5f7574fc72138cdd2922ff
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-07-27 21:01:43 +0530

    new

--- Frontend/src/features/interview/hooks/useInterview.js
+++ Frontend/src/features/interview/hooks/useInterview.js
@@ -2,6 +2,7 @@ import {
   getAllInterviewReports,
   generateInterviewReport,
   getInterviewReportById,
+   generateResumePdf
 } from "../services/interview.api";
 import { useContext, useEffect } from "react";
 import { InterviewContext } from "../interview.context";
@@ -70,25 +71,25 @@ export const useInterview = () => {
     return response.interviewReports;
   };
 
-//   const getResumePdf = async (interviewReportId) => {
-//     setLoading(true);
-//     let response = null;
-//     try {
-//       response = await generateResumePdf({ interviewReportId });
-//       const url = window.URL.createObjectURL(
-//         new Blob([response], { type: "application/pdf" }),
-//       );
-//       const link = document.createElement("a");
-//       link.href = url;
-//       link.setAttribute("download", `resume_${interviewReportId}.pdf`);
-//       document.body.appendChild(link);
-//       link.click();
-//     } catch (error) {
-//       console.log(error);
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
+  const getResumePdf = async (interviewReportId) => {
+    setLoading(true);
+    let response = null;
+    try {
+      response = await generateResumePdf({ interviewReportId });
+      const url = window.URL.createObjectURL(
+        new Blob([response], { type: "application/pdf" }),
+      );
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute("download", `resume_${interviewReportId}.pdf`);
+      document.body.appendChild(link);
+      link.click();
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
     if (interviewId) {
@@ -105,6 +106,6 @@ export const useInterview = () => {
     generateReport,
     getReportById,
     getReports,
-    // getResumePdf,
+    getResumePdf,
   };
 };
