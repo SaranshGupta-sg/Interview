de480efb28af2fe936ec8e7ce8539f70ba58b7ec
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-07-30 21:50:34 +0530

    new

--- Backend/server.js
+++ Backend/server.js
@@ -7,6 +7,8 @@ dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 connectToDB();
 
+app.get("/", (req, res) => res.send("API Working Successfully"));
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
