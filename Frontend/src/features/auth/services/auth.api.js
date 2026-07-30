a3cc6c35d2f74de0b19dde17cb8a77108ed9a545
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-07-30 21:54:56 +0530

    new

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://interview-backend-2kym.onrender.com",
   withCredentials: true,
 });
 
